10d48bce5e8eb720c5c0420603a291ad691f7c07
oussama yousr <47725118+Oussamayousre@users.noreply.github.com>
2020-11-22 00:18:02 +0100

    Update convert_Pdf_To_Audio.py

--- convert_Pdf_To_Audio.py
+++ convert_Pdf_To_Audio.py
@@ -1,3 +1,6 @@
+#pyttsx3 is a text-to-speech conversion library in Python. Unlike alternative libraries, it works offline, and is compatible with both Python 2 and 3.
+#pdfplumber : Plumb a PDF for detailed information about each text character, rectangle, and line. Plus: Table extraction and visual debugging
+#pypdf2 : PyPDF2 is a pure-python PDF library capable of splitting, merging together, cropping, and transforming the pages of PDF files
 import pyttsx3
 from PyPDF2 import PdfFileReader
 import pdfplumber
